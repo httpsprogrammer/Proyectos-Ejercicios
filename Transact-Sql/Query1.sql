1d83a6de08355fe498a76bae6c94526ac6cf6a3e
ALEX VALLEJO LLIVICURA <httpsprogrammer@outlook.com>
2019-10-08 21:35:02 +0200

    Añado querys

--- Transact-Sql/Query1.sql
+++ Transact-Sql/Query1.sql
@@ -1,2 +1,11 @@
 /*Crea una base de datos*/
 create database PruebaBBDD;
+
+/*Use y el nombre de la base de datos que se va a usar la query*/
+Use PruebaBBDD
+/*Crea una tabla con un Id */
+create table dbo.Persona
+(IdPersona int Primary key not null);
+
+/*Elimina una tabla de la base de datos*/
+DROP TABLE dbo.Persona; 
